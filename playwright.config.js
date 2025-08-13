8f5aaa296bd88b555c322e573953fb8bccfe0a0e
rfigueroa <rfigueroa@IT-CR-ZF-0547.local>
2025-08-12 19:45:05 -0600

    Allure reports

--- playwright.config.js
+++ playwright.config.js
@@ -21,7 +21,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["line"], ["allure-playwright", {
+    resultsDir: 'allure-assets'
+  }]],
   expect: {
     timeout: 1000,
   },
